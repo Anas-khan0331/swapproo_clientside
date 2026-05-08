476532080c83b0bec2d2a0c775018f43f332e285
Anas_khan <77989560+Anas-khan0331@users.noreply.github.com>
2026-05-08 21:37:38 +0500

    Fix breadcrumb.tsx Slot.Root to Slot

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -40,7 +40,7 @@ function BreadcrumbLink({
 }: React.ComponentProps<"a"> & {
   asChild?: boolean;
 }) {
-  const Comp = asChild ? Slot.Root : "a";
+  const Comp = asChild ? Slot : "a";
 
   return (
     <Comp
