e60f6a212cf6908fd9e5c3914f24c0605b1f695a
Anas_khan <77989560+Anas-khan0331@users.noreply.github.com>
2026-05-11 13:59:48 +0500

    Show static Help Center UI even when API fails

--- src/app/help-center/page.tsx
+++ src/app/help-center/page.tsx
@@ -1,10 +1,16 @@
-import HelpCenterHome from "@/features/help-center";
+import HelpByCategory from "@/features/help-center/components/HelpByCategory";
+import HelpCenterHero from "@/features/help-center/components/HelpCenterHero";
+import HelpQuickAssistance from "@/features/help-center/components/HelpQuickAssistance";
 import { fetchCategories } from "@/store/help-center";
 
 export default async function HelpCenterHomePage() {
   const [err, categories] = await fetchCategories();
 
-  if (err) return;
-
-  return <HelpCenterHome categories={categories} />;
+  return (
+    <>
+      <HelpCenterHero />
+      {!err && categories && <HelpByCategory categories={categories} />}
+      <HelpQuickAssistance />
+    </>
+  );
 }
